ed496ed6d7e102daa0ea56538b8300328beaa870
sahil <sahilch0407@gmail.com>
2025-06-26 06:33:58 +0530

    add new file

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+  new Typed(".section__text__p2", {
+    strings: ["Front-End Developer","Backend-End Developer"],
+    typeSpeed: 150,
+    backSpeed: 60,
+    backDelay: 1500,
+    loop: true
+  });
