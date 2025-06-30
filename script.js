821af535e06f2daa7738f63efde126483b118372
sahil <sahilch0407@gmail.com>
2025-07-01 02:45:59 +0530

    changed

--- script.js
+++ script.js
@@ -54,12 +54,14 @@ window.addEventListener("load", () => {
     icon.classList.add("fa-sun");
   }
 });
-  window.addEventListener("scroll", () => {
-    const scrollTop = window.scrollY;
-    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
-    const scrollPercent = (scrollTop / docHeight) * 100;
-    document.getElementById("scroll-progress").style.width = scrollPercent + "%";
-  });
+window.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+  document.getElementById("scroll-progress").style.width = scrollPercent + "%";
+});
+
+
 
 
 
